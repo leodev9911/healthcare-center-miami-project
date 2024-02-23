2a79a3ed254c7e74df4b2c455dc735ed0ca74b78
Leonardo Fernández <leonardo99112821385@gmail.com>
2024-02-23 02:21:39 -0500

    Footer ready

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,5 +1,57 @@
+import logoImage from '../assets/logo.png'
+import spainFlag from '../assets/spain-flag.png'
+import arrowDown from '../assets/arrow-down.svg'
+
 export default function Footer() {
   return (
-    <h2>Footer</h2>
+    <footer className="flex flex-col gap-11 items-center bg-blue-950 py-9 mt-[160px]">
+      <nav className="w-full max-w-5xl flex flex-col lg:flex-row gap-6 items-center justify-between px-2 z-30 relative">
+        <img 
+          className='w-[248px] h-[43px]'
+          src={logoImage} 
+          alt="Logo"   
+        />
+        <section>
+          <ul className='text-white flex flex-col gap-8 lg:flex-row items-center lg:gap-20'>
+            <li>
+              <a 
+                href=""
+                className='cursor-pointer hover:underline duration-300'
+              >
+                Nuestra Compañía
+              </a>
+            </li>
+            <li>
+              <a 
+                href=""
+                className='cursor-pointer hover:underline duration-300'
+              >
+                Nuestros Servicios
+              </a>
+            </li>
+            <li className='flex items-center gap-3'>
+              <img 
+                src={spainFlag} 
+                alt="Spain flag" 
+              />
+              <img 
+                src={arrowDown} 
+                alt="Arrow down"
+                className='cursor-pointer' 
+              />
+            </li>
+            <li>
+              <a 
+                href=""
+                className='cursor-pointer hover:underline duration-300'
+              >
+                Contáctanos
+              </a>
+            </li>
+          </ul>
+        </section>
+      </nav>
+      <p className='text-white'>© 2024 Moya Medical. Todos los derechos reservados.</p>
+    </footer>
   )
 }
